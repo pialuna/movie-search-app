8e57b55188b2aa666352b703f5b119b0f9f0d801
pialuna <pia.goldm@gmail.com>
2021-05-06 23:59:47 +0200

    :construction: Style, navbar, search input

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,28 @@
-import "./App.css";
+import Navbar from "./components/Navbar";
 import data from "./dummyData";
 
 function App() {
-  return <div className="App">{data.toString()}</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="m-5 flex flex-col items-center">
+        <h1>Movies</h1>
+        <h5 className="text-gray-400">
+          (Movies from 2001 whose title contains "Space")
+        </h5>
+        <label className="sr-only" for="search">
+          Search for movies
+        </label>
+        <input
+          className="w-6/12 bg-white border-2 rounded-md shadow-sm p-2 my-2 flex flex-col items-start"
+          type="text"
+          id="search"
+          name="search"
+          placeholder="Search"
+        ></input>
+      </div>
+    </>
+  );
 }
 
 export default App;
