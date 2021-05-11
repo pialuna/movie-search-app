3ab85f217b17b22cb15f1c826babb863637cf26a
pialuna <pia.goldm@gmail.com>
2021-05-11 15:44:42 +0200

    :card_file_box: add mongodb

--- api/src/routes/movies.js
+++ api/src/routes/movies.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
 const Movie = require("../models/movie");
 
